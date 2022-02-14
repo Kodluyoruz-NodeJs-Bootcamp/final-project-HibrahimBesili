0f4adf34ac6b870b02d9f069804a87cce48779aa
HibrahimBesili <75371431+HibrahimBesili@users.noreply.github.com>
2022-02-14 03:46:29 +0300

    Delete build directory

--- build/constants/enums.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var PostType;
-(function (PostType) {
-    PostType[PostType["Movie"] = 1] = "Movie";
-    PostType[PostType["Actor_Actress"] = 2] = "Actor_Actress";
-})(PostType = exports.PostType || (exports.PostType = {}));
-//# sourceMappingURL=enums.js.map
